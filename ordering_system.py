aa18e33c9e818dada7c5c3fe706256d0c7cddcd4
ME <amountii00@gmail.com>
2022-09-23 10:44:25 +0200

    Update ordering_system.py

--- ordering_system.py
+++ ordering_system.py
@@ -1,3 +1,8 @@
+"""
+In this lab you will be presented with a menu ordering system which will allow users to   
+input three choices for a select menu. You are tasked with completing the menu system so   
+that it returns and calculates the final bill for the user.
+"""
 menu = {
     1: {"name": 'espresso',
         "price": 1.99},
